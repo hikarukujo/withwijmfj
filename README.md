c08cacb14eb254b6d94e47485fa504f802c38b25
Joe Johnson <joe.johnson@revalizesoftware.com>
2023-07-21 21:19:40 +0200

    Initial Load

---
+++ README.md
@@ -0,0 +1,3 @@
+# Where in the World is Joe Mother Fucking Johnson
+
+An app to track me, and show where I am at all times.
---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const latitude = 50.052430;
+const longitude = 8.572340;
+
+const App = () => {
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${process.env.REACT_APP_OPENCAGE_API_KEY}`);
+        const data = response.data;
+        if (data.results && data.results.length > 0) {
+          const locationDetails = data.results[0].components;
+          setLocation({
+            city: locationDetails.city,
+            state: locationDetails.state,
+            country: locationDetails.country
+          });
+        }
+      } catch (error) {
+        console.error(`Error: ${error}`);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h2 style={{ textAlign: 'center', fontWeight: 'bold' }}>
+            {location.city}, {location.state}
+            <br />
+            {location.country}
+          </h2>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default App;
